3a14257a0e6268842e6a081cfc6efa419ec5e410
theparthshira <shirawalaparth@gmail.com>
2023-05-29 12:05:02 +0530

    new

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 app.post("/test", (req, res) => {
   const name = req.body;
-  console.log("name =====", name);
+  console.log("name =====", JSON.stringify(req.body));
   res.send(name);
 });
 
